7a9be3e78b151743105fb64cf0ce1ef3d1ea85aa
davre001 <davemode96@gmail.com>
2026-07-01 07:38:02 +0100

    layout fix

--- docs-site/src/app/(docs)/layout.tsx
+++ docs-site/src/app/(docs)/layout.tsx
@@ -14,7 +14,7 @@ export default async function DocsLayout({ children }: { children: React.ReactNo
   return (
     <Layout
       banner={<Banner storageKey="shieldpass-banner"><span className="banner-typing">ShieldPass V2 is here!</span></Banner>}
-      navbar={<Navbar logo={<span />} projectLink={null} />}
+      navbar={<Navbar logo={<span />} />}
       pageMap={pageMap}
       docsRepositoryBase="https://github.com/ShieldPass/shieldpass/tree/main/docs-site"
       footer={<Footer>MIT {new Date().getFullYear()} © ShieldPass.</Footer>}
